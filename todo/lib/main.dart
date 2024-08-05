41ebe89cf2c4d9ff31165945c5a7bce2a98dfa75
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-08-05 14:29:52 +0500

    App Icon Updated

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -11,6 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: "TODO App",
       home: FirstScreen(),
       debugShowCheckedModeBanner: false,
     );
